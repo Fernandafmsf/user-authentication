3d9d516db6073e6444e1e1513e4593778cd4e668
Fernandafmsf <fernandafernandesauto@gmail.com>
2026-06-23 19:50:25 -0300

    fix: adjust route to find all users if user is admin

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { createUser, findAll, deleteUser, returnUser, updateUser} from '../modules/user/userController.js';
+import {
+  createUser,
+  findAll,
+  deleteUser,
+  returnUser,
+  updateUser,
+} from "../modules/user/userController.js";
 import auth from "../middleware/auth.js";
 import isAdmin from "../middleware/isAdmin.js";
 import { login } from "../modules/auth/authService.js";
@@ -7,12 +13,11 @@ import { login } from "../modules/auth/authService.js";
 const router = express.Router();
 
 router.post("/register", createUser);
+
+router.get("/admin", isAdmin, findAll);
+
+router.get("/:id", auth, returnUser);
 router.post("/:id", auth, updateUser);
 router.delete("/:id", auth, deleteUser);
-
-// router.get("/admin", isAdmin, findAll);
-router.get("/:id", auth, returnUser);
-
-
 
 export default router;
